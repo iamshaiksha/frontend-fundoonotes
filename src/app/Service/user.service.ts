a39173656b12c6c00b43533936dd68ca2f3a622b
iamshaiksha <iamshaiksha2019@gmail.com>
2020-03-21 20:35:17 +0530

    Login and registration,validation and integration with backend implemented

--- src/app/Service/user.service.ts
+++ src/app/Service/user.service.ts
@@ -17,31 +17,7 @@ export class UserService {
     return this.http.post("http://localhost:8080/user/Registration",user,{responseType:'text' as 'json'});
     
   }
-  // public doLogin(user:any)
-  // {
-  //   console.log("Login in user"+user)
-  //   return this.http.post("http://localhost:8080/user/Login",user,{responseType:'text' as 'json'});
-    
-  // }
-  // public postRequest(url:any,data:any):any{
-  //   return this.http.post(this.baseUrl+url,"",{headers:new HttpHeaders().set("jwtToken", localStorage.getItem('token'))});
-  //   }
-    
-  //   public putRequest(url:any):any{
-  //   return this.http.put(this.baseUrl+url,"",{headers:new HttpHeaders().set("jwtToken", localStorage.getItem('token'))});
-  //   }
-    
-    
-  //   public getRequest(url:any):any{
-  //   return this.http.get(this.baseUrl+url);
-  //   }
-    
-  //   public deleteRequest(url:any):any{
-  //   return this.http.delete(this.baseUrl+url);
-  //   }
-  //   public putRequestForget(url,data){
-  //       return this.http.put(this.baseUrl + url,data);
-  //     }
+  
     }
 
 
