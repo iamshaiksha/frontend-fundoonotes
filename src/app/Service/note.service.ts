a23206e0e0da9675aeb91a35e2549e17ff6a0764
iamshaiksha <iamshaiksha2019@gmail.com>
2020-04-03 23:15:59 +0530

    [shaiksha]refractor.notes,labels,hoovers,dashboard changes done

---
+++ src/app/Service/note.service.ts
@@ -0,0 +1,117 @@
+import { environment } from 'src/environments/environment';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable, Input } from '@angular/core';
+import {Observable} from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class Noteservice {
+  baseurl = environment.baseUrlNote;
+  baseurlLabel = environment.baseUrlLabel;
+/**
+ * 
+ * @param http 
+ * 
+ */
+  constructor(private http: HttpClient) { }
+
+  /**
+   * 
+   * @param url 
+   * @param data
+   * token passing through Httpheader snippet 
+   */
+  public postRequest(url: any, data: any): any {
+    return this.http.post(this.baseurl + url, data, { headers: new HttpHeaders().set('token', localStorage.getItem('token')) });
+  }
+  /**
+   * 
+   * @param url 
+   * @param data
+   * @Requestheader passing through Httpheader snippet 
+   */
+  public putRequest(url: any, data: any): any {
+    console.log(this.baseurl + url+data);
+    return this.http.put(this.baseurl + url, data,{ headers: new HttpHeaders().set('token', localStorage.getItem('token'))});
+  } 
+  /**
+   * 
+   * @param url 
+   * @param data
+   * @Requestheader passing through Httpheader snippet 
+   */
+  public deleteRequest(url: any,data:any): any {
+    console.log(localStorage.getItem('token'));
+    console.log(this.baseurl + url);
+    return this.http.put(this.baseurl + url,data,{headers:new HttpHeaders().set('token',localStorage.getItem('token')) });
+  }
+  /**
+   * 
+   * @param url 
+   * @Requestheader passing through Httpheader snippet
+   */
+  public getRequest(url: any): any {
+    return this.http.get(this.baseurl + url, { headers: new HttpHeaders().set('token', localStorage.getItem('token')) });
+  }
+  /**
+   * 
+   * @param url 
+   * @param data
+   *  
+   */
+  public deletenoteRequest(url:any,data:any):any
+  {
+    return this.http.put(this.baseurl +url,localStorage.getItem("token"));
+  }
+  /**
+   * 
+   * @param url 
+   * @Requestheader passing through Httpheader snippet
+   */
+  public deletearchieveRequest(url: any): any {
+    return this.http.post(this.baseurl + url, { headers: new HttpHeaders().set('token', localStorage.getItem('token')) });
+  }
+  // public deleteRequest(url: any):Observable<any> 
+  //  {
+  //   // return this.http.put(this.baseurl + url,{headers:new HttpHeaders().set('token',localStorage.getItem('token')) });
+  // const token={
+  //   headers:new HttpHeaders({'X-Requested-with':localStorage.getItem('token')})
+  // };
+  // console.log(token.headers.get('X-Requested-with'));
+  // return this.http.put(this.baseurl + url,token);
+ 
+  // }
+  // public deleteLabelRequest(url: any,data:any): any {
+  //   return this.http.delete(this.baseurlLabel + url, data,{ headers: new HttpHeaders().set('token', localStorage.getItem('token'))});
+  // }
+  /**
+   * 
+   * @param url 
+   * @param data
+   * @Requestheader passing through Httpheader snippet 
+   */
+  public deleteLabelRequest(url: any,data:any): any {
+    console.log(localStorage.getItem('token'));
+    console.log(this.baseurl + url);
+    return this.http.post(this.baseurlLabel + url,data,{headers:new HttpHeaders().set('token',localStorage.getItem('token'))});
+  }
+  /**
+   * 
+   * @param url 
+   * @param data 
+   * @Requestheader passing through Httpheader snippet
+   */
+  public deletepinRequest(url: any,data:any): any {
+    return this.http.put(this.baseurl + url, data,{ headers: new HttpHeaders().set('token', localStorage.getItem('token')) });
+  }
+  /**
+   * 
+   * @param url 
+   * @param data
+   *  
+   */
+  public putupdateRequest(url: any,data:any): any {
+    return this.http.put(this.baseurl + url,data);
+  }
+}
