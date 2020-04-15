844d1880c454474d8a18c0bb7e922dab45821d25
iamshaiksha <iamshaiksha2019@gmail.com>
2020-04-15 21:22:13 +0530

    refractor.changes of profile,collaborator,reminder done

--- src/app/Service/user.service.ts
+++ src/app/Service/user.service.ts
@@ -8,6 +8,7 @@ import { User } from '../model/User';
 import { environment } from 'src/environments/environment';
 import { Login } from '../model/login';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -31,14 +32,26 @@ export class UserService {
    */
   public doRegistration(user: any) {
     console.log("checking in service" + user)
-    return this.http.post("http://localhost:8080/user/Registration", user, { responseType: 'text' as 'json' });
-
+    return this.http.post("http://localhost:8090/user/Registration", user, { responseType: 'text' as 'json' });
   }
-
-
   public putRequestForget(url, data: any) {
     const newLocal = this.baseUrl;
-    return this.http.post("http://localhost:8080/user/forgetPassword/" + url, data);
+    return this.http.post("http://localhost:8090/user/forgetPassword/" + url, data);
   }
-
+  getUser(userName:String)
+  {
+    console.log("getting single user")
+    return this.http.get<User>("http://localhost:8090/user/singleUser/",{ headers: new HttpHeaders().set('token', localStorage.getItem('token')) });
+  }
+  // getUserInformation(url:any)
+  // {
+  //   console.log("shaiksha----->geeting user")
+  //   return this.http.get("http://localhost:8080/" + url);
+  // }
+  // getAllUsers
+  getAllUsers(url:any)
+  {
+    console.log("getting All users ")
+    return this.http.get("http://localhost:8080/user/allUsers/") ;
+  }
 }
