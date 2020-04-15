299ebd50f48997aa79af8955e8d32d3b0f08f906
iamshaiksha <iamshaiksha2019@gmail.com>
2020-04-15 21:35:05 +0530

    refractor.changes ofaddlabels, profile,collaborator,reminder,elastic search done

--- src/app/component/icon/icon.component.ts
+++ src/app/component/icon/icon.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { Note } from 'src/app/model/note';
 import { Noteservice } from 'src/app/Service/note.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Color } from 'src/app/model/setcolor';
 import { DataService } from 'src/app/Service/data.service';
@@ -11,11 +11,42 @@ import { Trash } from 'src/app/model/trash';
 import { HttpService } from 'src/app/Service/http.service';
 import { LabelService } from 'src/app/Service/label.service';
 import { LabelNote } from 'src/app/model/labelnote';
+import { LabelsComponent } from '../labels/labels.component';
+import { ReminderComponent } from '../reminder/reminder.component';
+import { Reminder } from 'src/app/model/reminder';
+import { BrowserModule } from '@angular/platform-browser';
+import * as _moment from 'moment';
+import { DateTimeAdapter, OWL_DATE_TIME_FORMATS, OWL_DATE_TIME_LOCALE } from 'ng-pick-datetime';
+import { CollaboratorComponent } from '../collaborator/collaborator.component';
+// import { DateTimeAdapter, OWL_DATE_TIME_FORMATS, OWL_DATE_TIME_LOCALE } from 'ng-pick-datetime';
+// import { MomentDateTimeAdapter } from 'ng-pick-datetime-moment';
+
+const moment = (_moment as any).default ? (_moment as any).default : _moment;
+
+export const MY_CUSTOM_FORMATS = {
+  parseInput: 'LL LT',
+  fullPickerInput: 'LL LT',
+  datePickerInput: 'LL',
+  timePickerInput: 'LT',
+  monthYearLabel: 'MMM YYYY',
+  dateA11yLabel: 'LL',
+  monthYearA11yLabel: 'MMMM YYYY',
+};
 
 @Component({
   selector: 'app-icon',
   templateUrl: './icon.component.html',
-  styleUrls: ['./icon.component.css']
+  styleUrls: ['./icon.component.css'],
+  // changeDetection:ChangeDetectionStrategy.OnPush,
+ 
+  providers:[
+
+    // {provide: DateTimeAdapter, useClass: MomentDateTimeAdapter, deps: [OWL_DATE_TIME_LOCALE]},
+
+        {provide: OWL_DATE_TIME_FORMATS, useValue: MY_CUSTOM_FORMATS},
+  ]
+
+
 })
 export class IconComponent implements OnInit {
   /**
@@ -33,6 +64,7 @@ export class IconComponent implements OnInit {
   token: String;
   @Input() noteInfo: any;
   trash: Trash = new Trash();
+  public dateTime = new moment();
   /**
    * 
    * @param snackbar 
@@ -51,16 +83,16 @@ export class IconComponent implements OnInit {
     private formBuilder: FormBuilder,
     private snackbar: MatSnackBar,
     private dialog: MatDialog,
-    private httpservice: HttpService
-  ) 
-  {
+    private httpservice: HttpService,
+    private data:DataService
+  ) {
 
   }
-/**
- * while loading icon component getting token
- * and getting allnotes
- * autorefreshing
- */
+  /**
+   * while loading icon component getting token
+   * and getting allnotes
+   * autorefreshing
+   */
   ngOnInit(): void {
     this.token = localStorage.getItem("token");
     this.dataservice.currentMessage.subscribe(
@@ -69,9 +101,9 @@ export class IconComponent implements OnInit {
       }
     )
   }
-/**
- * fetching all labels based upon userid
- */
+  /**
+   * fetching all labels based upon userid
+   */
   getalllabels() {
     console.log("###########")
     this.labelservice.getRequest("/getLabelsByUserId/" + localStorage.getItem("token")).subscribe(
@@ -101,6 +133,18 @@ export class IconComponent implements OnInit {
       { name: "white", hexcode: " #ffffff " }, { name: "cyan", hexcode: " #00FFFF " }, { name: "red", hexcode: "#ff0000" }
     ]
   ]
+  onCollaboraotr()
+  {
+    const dialogRef = this.dialog.open(CollaboratorComponent, {
+      width: '800px',
+      data:{
+        'noteId': this.noteInfo.noteId
+      }
+    });
+  }
+
+
+
   /**
    * 
    * @param color
@@ -174,14 +218,12 @@ export class IconComponent implements OnInit {
       }
     )
   }
-/**
- * when ever click on delete the note automatically goes to trash 
- */
+  /**
+   * when ever click on delete the note automatically goes to trash 
+   */
   onDelete() {
     console.log(this.noteInfo.noteId);
-    this.trash.nid = this.noteInfo.noteId;
     this.token = localStorage.getItem("token");
-    console.log("#####---trash==" + this.trash.nid)
     this.noteService.deleteRequest("delete?noteId=" + this.noteInfo.noteId, "").subscribe(
       (Response: any) => {
 
@@ -228,7 +270,7 @@ export class IconComponent implements OnInit {
   //     }
   //   )
   // }
-  
+
   /**
    * 
    * @param labels 
@@ -244,7 +286,7 @@ export class IconComponent implements OnInit {
     this.labelnote.labelId = labels.labelId;
     this.labelnote.noteId = this.noteInfo.noteId;
     console.log("###############");
-       this.noteInfo.labelList.forEach(ele => {
+    this.noteInfo.labelList.forEach(ele => {
       if (ele.labelId == labels.labelId) {
         this.enable = false;
       }
@@ -282,4 +324,50 @@ export class IconComponent implements OnInit {
         { duration: 2500 })
     }
   }
+  // reminderOn(): void
+  // {
+  //   const dialogRef = this.dialog.open( ReminderComponent , {
+  //     width: '250px',
+  // });}
+  // openDialogLabel(): void {
+  //   const dialogRef = this.dialog.open(LabelsComponent, {
+  //     width: '250px',
+  //   });
+  // }
+  // }
+
+  dateReminder: Reminder = new Reminder();
+  dateControl = new FormControl(this.dateReminder.remainder);
+
+
+
+  datePicker() {
+    console.log(this.dateTime._d)
+    this.dateReminder.remainder = this.dateTime._d;
+    this.dateReminder.noteId = this.noteInfo.noteId;
+    console.log("noteid--->"+this.noteInfo.noteId);
+    console.log(this.dateReminder)
+    this.noteService.postRequest("addremainder/"+ localStorage.getItem("token"), this.dateReminder).subscribe(
+      (Response: any) => {
+        if (Response!=null) {
+          console.log(Response)
+          this.data.changeMessage("addRemainder")
+          this.snackbar.open(
+            "Reminder Successfull", "",
+            { duration: 2500 }
+          )
+        }
+        else {
+          console.log(Response)
+          this.snackbar.open(
+            "Reminder UnSuccessfull", "",
+            { duration: 2500 }
+          )
+        }
+      }
+    )
+  }
+
+
+
 }
